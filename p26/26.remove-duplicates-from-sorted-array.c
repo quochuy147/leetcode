cad2933af1970d7adbc719f29a5db48f76eb3101
quochuy147 <vquochuy84@gmail.com>
2021-07-27 22:32:28 +0700

    p26

---
+++ p26/26.remove-duplicates-from-sorted-array.c
@@ -0,0 +1,9 @@
+int removeDuplicates(int* nums, int numsSize){
+    if(numsSize == 0) return 0;
+    int k = 1;
+    for(int i = 1; i < numsSize; ++i) {
+        if(nums[i] == nums[i - 1]) continue;
+        nums[k++] = nums[i];
+    }
+    return k;
+}
