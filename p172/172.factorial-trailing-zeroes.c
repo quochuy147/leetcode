9a6658d43595bfbb93f05a77e9489e679246b926
quochuy147 <vquochuy84@gmail.com>
2021-08-02 23:41:49 +0700

    Done p172

---
+++ p172/172.factorial-trailing-zeroes.c
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/factorial-trailing-zeroes/
+
+int trailingZeroes(int n){
+    int res = 0;
+    while(n >= 5) {
+        n /= 5;
+        res += n;
+    }
+    return res;
+}
