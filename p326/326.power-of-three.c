6c1d844f204305e4dd104667580370bf0320673c
quochuy147 <vquochuy84@gmail.com>
2021-08-18 22:25:53 +0700

    Done p326

---
+++ p326/326.power-of-three.c
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/power-of-three/
+
+bool isPowerOfThree(int n) {
+    if(n <= 0) return false;
+    while(n % 3 == 0) {
+        n  /= 3;
+    }
+    if(n == 1) return true;
+    return false;
+}
